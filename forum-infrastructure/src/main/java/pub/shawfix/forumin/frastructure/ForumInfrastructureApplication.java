627c3654f1cf0ea8596f9467c4a8e72056ff3c87
Shaw996 <shawliu996@gmail.com>
2025-04-21 18:06:53 +0800

    添加forum-infrastructure模块

---
+++ forum-infrastructure/src/main/java/pub/shawfix/forumin/frastructure/ForumInfrastructureApplication.java
@@ -0,0 +1,13 @@
+package pub.shawfix.forumin.frastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumInfrastructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumInfrastructureApplication.class, args);
+	}
+
+}
