ecb1e436e29fa38efd5fca1294d73e76129d9901
Shaw996 <shawliu996@gmail.com>
2025-04-21 15:28:23 +0800

    feat: 添加forum-facade模块

---
+++ forum-facade/src/main/java/pub/shawfix/forum/facade/ForumFacadeApplication.java
@@ -0,0 +1,13 @@
+package pub.shawfix.forum.facade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumFacadeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ForumFacadeApplication.class, args);
+  }
+
+}
