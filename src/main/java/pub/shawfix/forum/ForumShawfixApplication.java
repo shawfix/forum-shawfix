30de0ce40567ed5892e169079b74a0b1e634ed39
Shaw996 <shawliu996@gmail.com>
2025-04-21 14:10:12 +0800

    first commit

---
+++ src/main/java/pub/shawfix/forum/ForumShawfixApplication.java
@@ -0,0 +1,13 @@
+package pub.shawfix.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumShawfixApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ForumShawfixApplication.class, args);
+  }
+
+}
---
+++ src/test/java/pub/shawfix/forum/ForumShawfixApplicationTests.java
@@ -0,0 +1,13 @@
+package pub.shawfix.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumShawfixApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
