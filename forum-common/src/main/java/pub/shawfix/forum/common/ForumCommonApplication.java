e703df1b67b47419d69dde041b1440b05b8b416c
Shaw996 <shawliu996@gmail.com>
2025-04-21 17:02:11 +0800

    feat: 添加forum-domain模块

---
+++ forum-common/src/main/java/pub/shawfix/forum/common/ForumCommonApplication.java
@@ -0,0 +1,13 @@
+package pub.shawfix.forum.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumCommonApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ForumCommonApplication.class, args);
+  }
+
+}
