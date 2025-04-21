b825a5c340535cecaca870cd0c5be1879b297f4c
Shaw996 <shawliu996@gmail.com>
2025-04-21 17:19:56 +0800

    feat: 添加forum-portal模块

---
+++ forum-portal/src/main/java/pub/shawfix/forum/portal/ForumPortalApplication.java
@@ -0,0 +1,13 @@
+package pub.shawfix.forum.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumPortalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ForumPortalApplication.class, args);
+  }
+
+}
