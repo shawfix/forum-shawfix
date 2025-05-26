ae7c394e428bd757d97aadab25113e6ba62d7f68
Shawfix <shawliu996@gmail.com>
2025-05-26 14:32:38 +0800

    feat: 更新start模块

--- forum-starter/src/main/java/pub/shawfix/forum/starter/ForumStarterApplication.java
+++ forum-starter/src/main/java/pub/shawfix/forum/starter/ForumStarterApplication.java
@@ -1,13 +1,26 @@
 package pub.shawfix.forum.starter;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
-public class ForumStarterApplication {
+@EnableScheduling
+@SpringBootApplication(scanBasePackages = "pub.shawfix.forum")
+@MapperScan(value = {"pub.shawfix.forum.infrastructure.dal.dao"})
+// TODO: SpringBootServletInitializer类是做什么的
+public class ForumStarterApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(ForumStarterApplication.class, args);
+  }
+
+  // TODO: 重写的意义
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(ForumStarterApplication.class);
   }
 
 }
